a39a008cd46a480ebcc23b01e64c475c28778fb3
Chris Wilson <cwilso@gmail.com>
2013-01-08 11:36:59 -0800

    Changed lookahead times, fixed drawing routine.

--- js/metronome.js
+++ js/metronome.js
@@ -2,12 +2,10 @@ var audioContext = null;
 var isPlaying = false;		// Are we currently playing?
 var startTime;				// The start time of the entire sequence.
 var current16thNote;		// What note is currently last scheduled?
-var currentNoteStartTime;	// When does the last currently scheduled note 
-							// start playing? Only used for visual tracking.
 var tempo = 120.0;			// tempo (in beats per minute)
-var lookahead = 50.0;		// How frequently to call scheduling function 
+var lookahead = 25.0;		// How frequently to call scheduling function 
 							//(in milliseconds)
-var scheduleAhead = 1.5*lookahead/1000;	// How far ahead to schedule audio (sec)
+var scheduleAheadTime = 0.1;	// How far ahead to schedule audio (sec)
 							// This is calculated from lookahead, and overlaps 
 							// with next interval (in case the timer is late)
 var nextNoteTime = 0.0;		// when the next note is due.
@@ -18,6 +16,8 @@ var intervalID = 0;			// setInterval identifier.
 var canvas,       			// the canvas element
     canvasContext;  		// canvasContext is the canvas' context 2D
 var last16thNoteDrawn = -1;	// the last "box" we drew on the screen
+var notesInQueue = [];      // the notes that have been put into the web audio,
+                            // and may or may not have played yet. {note, time}
 
 // First, let's shim the requestAnimationFrame API, with a setTimeout fallback
 window.requestAnimFrame = (function(){
@@ -44,7 +44,8 @@ function nextNote() {
 }
 
 function scheduleNote( beatNumber, time ) {
-	currentNoteStartTime = time;	// keeps track in order to show visuals.
+    // push the note on the queue, even if we're not playing.
+    notesInQueue.push( { note: beatNumber, time: time } );
 
 	if ( (noteResolution==1) && (beatNumber%2))
 		return;	// we're not playing non-8th 16th notes
@@ -69,7 +70,7 @@ function scheduleNote( beatNumber, time ) {
 function scheduler() {
 	// while there are notes that will need to play before the next interval, 
 	// schedule them and advance the pointer.
-	while (nextNoteTime < audioContext.currentTime + scheduleAhead ) {
+	while (nextNoteTime < audioContext.currentTime + scheduleAheadTime ) {
 		scheduleNote( current16thNote, nextNoteTime );
 		nextNote();
 	}
@@ -100,18 +101,13 @@ function resetCanvas (e) {
 }
 
 function draw() {
-    var currentNote;
+    var currentNote = last16thNoteDrawn;
+    var currentTime = audioContext.currentTime;
 
-    // Check to make sure that the next note has actually started playing
-    // (Note that we're checking against the live audio clock here.)
-    if (audioContext.currentTime >= currentNoteStartTime )
-        currentNote = current16thNote - 1;
-    else  // if it hasn't started playing, just draw the last note.
-        currentNote = last16thNoteDrawn;
-
-    // Make sure note number is in range [0,15]
-    if (currentNote < 0)
-        currentNote = 15;
+    while (notesInQueue.length && notesInQueue[0].time < currentTime) {
+        currentNote = notesInQueue[0].note;
+        notesInQueue.splice(0,1);   // remove note from queue
+    }
 
     // We only need to draw if the note has moved.
     if (last16thNoteDrawn != currentNote) {
