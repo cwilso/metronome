b6e96e25e61dc67a4694dd08d5320326336d3587
Pomax <pomax@nihongoresources.com>
2022-03-14 10:04:28 -0700

    Update test.js

--- js/test.js
+++ js/test.js
@@ -6,13 +6,12 @@ const timerWorker = new Worker("js/andback.js");
 
 timerWorker.onmessage = (e) => {
   // perform some light computation
-  tryIncrement();
+  tryIncrement(Date.now());
   // then immediately tell the worker to round-trip a tick
   timerWorker.postMessage(MORE);
 };
 
-async tryIncrement() {
-  now = Date.now();
+async function tryIncrement(now) {
   if (now - last >= 1) {
     last = now;
     a = a + 1;
