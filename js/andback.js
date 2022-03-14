b3014ec6d8162d2faecb66692df0ea417a882695
Pomax <pomax@Pomaxs-MacBook-Pro.local>
2022-03-13 22:32:16 -0700

    more test

--- js/andback.js
+++ js/andback.js
@@ -1,9 +1,11 @@
 const TICK = { tick: true };
 let running = false;
 
+const tick = () => postMessage(TICK);
+
 // instant return loop, let's see if we can get more data than setInterval
 onmessage = function (e) {
     if (e.data.start) running = true;
     if (e.data.stop) running = false;
-    if (running) postMessage(TICK);
+    if (running) setTimeout(tick, 1);
 }
