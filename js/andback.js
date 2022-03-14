d50646e6e5e765fa8db79ef749c9fb928f6d0f5c
Pomax <pomax@Pomaxs-MacBook-Pro.local>
2022-03-13 22:50:06 -0700

    hm

--- js/andback.js
+++ js/andback.js
@@ -1,11 +1,9 @@
 const TICK = { tick: true };
 let running = false;
 
-const tick = () => postMessage(TICK);
-
 // instant return loop, let's see if we can get more data than setInterval
 onmessage = function (e) {
     if (e.data.start) running = true;
     if (e.data.stop) running = false;
-    if (running) setTimeout(tick, 1);
+    if (running) setTimeout(() => postMessage(TICK), 1);
 }
--- js/test.js
+++ js/test.js
@@ -1,16 +1,24 @@
 let a = 0;
-
+let last = 0;
+let now;
+const MORE = { more: true };
 const timerWorker = new Worker("js/andback.js");
 
 timerWorker.onmessage = (e) => {
-  a = a + 1;
-  timerWorker.postMessage({ more: true });
+  timerWorker.postMessage(MORE);
+  now = performance.now();
+  if (now - last > 0) {
+    last = now;
+    a = a + 1;
+  }
 };
 
+// start
 timerWorker.postMessage({ start: true });
+last = performance.now();
 
 setTimeout(() => {
-    timerWorker.postMessage({ stop: true });
-    console.log(a);
-    document.querySelector(`.drift`).textContent = a;
+  timerWorker.postMessage({ stop: true });
+  console.log(a);
+  document.querySelector(`.drift`).textContent = a;
 }, 1000);
